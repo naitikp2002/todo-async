8ccb225393ab4c7c91810532df70bbea3038c3d8
Naitik Patel <naitik2002patel@gmail.com>
2024-04-28 10:19:36 +0530

    fixed devider width to auto

--- src/components/Devider.js
+++ src/components/Devider.js
@@ -2,6 +2,7 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import ListItem from "./ListItem";
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 function JustifiedExample({ todos, setTodos }) {
   const [pendingTodos, setPendingTodos] = useState(null);
   const [completedTodos, setCompletedTodos] = useState(null);
@@ -11,22 +12,30 @@ function JustifiedExample({ todos, setTodos }) {
   useEffect(() => {
     setCompletedTodos(todos.filter((todo) => todo.isCompleted === true));
     setPendingTodos(todos.filter((todo) => todo.isCompleted === false));
-  }, [pendingTodos, completedTodos,todos]);
+  }, [pendingTodos, completedTodos, todos]);
 
   return (
     <div>
-      <Tabs 
-        style={{ width: "500px" }}
+      <Tabs
+        style={{ minWidth: "500px" }}
         defaultActiveKey="pending"
         id="justify-tab-example"
         className="mb-3"
         justify
       >
-        <Tab eventKey="pending" title="Pending">
-          <ListItem todos={pendingTodos} setTodos={setTodos} handleDelete={handleDelete}/>
-        </Tab>
+          <Tab eventKey="pending" title="Pending">
+            <ListItem
+              todos={pendingTodos}
+              setTodos={setTodos}
+              handleDelete={handleDelete}
+            />
+          </Tab>
         <Tab eventKey="completed" title="Completed">
-          <ListItem todos={completedTodos} setTodos={setTodos} handleDelete={handleDelete}/>
+          <ListItem
+            todos={completedTodos}
+            setTodos={setTodos}
+            handleDelete={handleDelete}
+          />
         </Tab>
       </Tabs>
     </div>
