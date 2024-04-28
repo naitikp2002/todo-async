a95b936f1571917b93103d6edbf3d30464244f19
Naitik Patel <naitik2002patel@gmail.com>
2024-04-28 19:28:51 +0530

    feat: json server for get data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import "./App.css";
 import InputForm from "./components/InputForm.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Lists from "./components/Lists.js";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import axios from "axios"
 import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [todos,setTodos]= useState([])
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get('http://localhost:3002/todos');
+      setTodos(response.data);
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  };
   return <>
     <InputForm todos={todos} setTodos={setTodos} />
     <Lists todos={todos} setTodos={setTodos}/>
